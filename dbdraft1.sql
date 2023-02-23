62a743ac74353910736f56599f1d9bee239539fd
MamoruTFN <78528062+MamoruTFN@users.noreply.github.com>
2023-02-23 23:22:04 +0700

    Update dbdraft1.sql

--- dbdraft1.sql
+++ dbdraft1.sql
@@ -19,6 +19,7 @@ CREATE TABLE `User`
  `userId`       int NOT NULL ,
  `userName`     varchar(45) NOT NULL ,
  `verifyStatus` enum('yes','no') NOT NULL ,
+ `imgPath`      varchar(150) NOT NULL ,
 
 PRIMARY KEY (`userId`),
 KEY `FK_1` (`userId`),
@@ -35,24 +36,25 @@ KEY `FK_1` (`staffId`),
 CONSTRAINT `FK_16` FOREIGN KEY `FK_1` (`staffId`) REFERENCES `Account` (`accountId`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `PostType`
+CREATE TABLE `Type`
 (
- `postTypeId` int NOT NULL ,
- `type`       varchar(45) NOT NULL ,
+ `typeId` int NOT NULL ,
+ `type`   varchar(45) NOT NULL ,
 
-PRIMARY KEY (`postTypeId`)
+PRIMARY KEY (`typeId`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `Post`
 (
  `postId`     int NOT NULL ,
  `userId`     int NOT NULL ,
- `postTypeId` int NULL ,
+ `typeId`     int NULL ,
  `postDetail` text NOT NULL ,
+ `imgPath`    varchar(150) NOT NULL ,
 
 PRIMARY KEY (`postId`),
-KEY `FK_3` (`postTypeId`),
-CONSTRAINT `FK_13` FOREIGN KEY `FK_3` (`postTypeId`) REFERENCES `PostType` (`postTypeId`) ON DELETE SET NULL ON UPDATE CASCADE,
+KEY `FK_3` (`typeId`),
+CONSTRAINT `FK_13` FOREIGN KEY `FK_3` (`typeId`) REFERENCES `Type` (`typeId`) ON DELETE SET NULL ON UPDATE CASCADE,
 KEY `FK_3_1` (`userId`),
 CONSTRAINT `FK_11` FOREIGN KEY `FK_3_1` (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -97,32 +99,51 @@ KEY `FK_3` (`userId`),
 CONSTRAINT `FK_12` FOREIGN KEY `FK_3` (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `CourseCreator`
+(
+ `courseCreatorId` int NOT NULL ,
+ `userId`          int NOT NULL ,
+ `balance`         float NOT NULL ,
+
+PRIMARY KEY (`courseCreatorId`),
+KEY `FK_2` (`userId`),
+CONSTRAINT `FK_28` FOREIGN KEY `FK_2` (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `Course`
 (
- `courseId`     int NOT NULL ,
- `postId`       int NULL ,
- `courseDetail` varchar(45) NOT NULL ,
- `minimum`      int NOT NULL ,
- `maximum`      int NULL ,
- `price`        float NOT NULL ,
- `status`       enum('full','available') NOT NULL ,
+ `courseId`        int NOT NULL ,
+ `createBy`        int NOT NULL ,
+ `typeId`          int NOT NULL ,
+ `postId`          int NULL ,
+ `courseDetail`    varchar(45) NOT NULL ,
+ `minimum`         int NOT NULL ,
+ `maximum`         int NULL ,
+ `price`           float NOT NULL ,
+ `status`          enum('full','available') NOT NULL ,
+ `firstEnrollDate` date NOT NULL ,
+ `lastEnrollDate`  date NOT NULL ,
+ `eventDay`        date NOT NULL ,
+ `startTime`       varchar(45) NOT NULL ,
+ `endTime`         varchar(45) NOT NULL ,
 
 PRIMARY KEY (`courseId`),
 KEY `FK_3` (`postId`),
-CONSTRAINT `FK_13_1` FOREIGN KEY `FK_3` (`postId`) REFERENCES `Post` (`postId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CONSTRAINT `FK_13_1` FOREIGN KEY `FK_3` (`postId`) REFERENCES `Post` (`postId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+KEY `FK_4` (`typeId`),
+CONSTRAINT `FK_25` FOREIGN KEY `FK_4` (`typeId`) REFERENCES `Type` (`typeId`) ON DELETE NO ACTION ON UPDATE CASCADE,
+KEY `FK_4_1` (`createBy`),
+CONSTRAINT `FK_28_1` FOREIGN KEY `FK_4_1` (`createBy`) REFERENCES `CourseCreator` (`courseCreatorId`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `JoinCourse`
 (
  `joinCourseId` int NOT NULL ,
- `userId`       int NOT NULL ,
  `courseId`     int NOT NULL ,
 
 PRIMARY KEY (`joinCourseId`),
 KEY `FK_3` (`courseId`),
-CONSTRAINT `FK_15` FOREIGN KEY `FK_3` (`courseId`) REFERENCES `Course` (`courseId`) ON DELETE CASCADE ON UPDATE CASCADE,
-KEY `FK_3_1` (`userId`),
-CONSTRAINT `FK_15_1` FOREIGN KEY `FK_3_1` (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+CONSTRAINT `FK_15` FOREIGN KEY `FK_3` (`courseId`) REFERENCES `Course` (`courseId`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `PostLike`
@@ -138,33 +159,34 @@ KEY `FK_3` (`userId`),
 CONSTRAINT `FK_10` FOREIGN KEY `FK_3` (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `Request`
+CREATE TABLE `RequestCourse`
 (
- `requestId`   int NOT NULL ,
- `requestBy`   int NOT NULL ,
- `postId`      int NOT NULL ,
- `approveBy`   int NOT NULL ,
- `status`      enum('waiting', 'approve', 'reject') NOT NULL ,
- `dateApprove` date NOT NULL ,
+ `requestCourseId` int NOT NULL ,
+ `requestBy`       int NOT NULL ,
+ `postId`          int NOT NULL ,
+ `approveBy`       int NOT NULL ,
+ `status`          enum('waiting', 'approve', 'reject') NOT NULL ,
+ `dateApprove`     date NOT NULL ,
+ `dateExprie`      date NOT NULL ,
 
-PRIMARY KEY (`requestId`),
+PRIMARY KEY (`requestCourseId`),
 KEY `FK_3` (`approveBy`),
-CONSTRAINT `FK_19` FOREIGN KEY `FK_3` (`approveBy`) REFERENCES `Staff` (`staffId`) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT `FK_19` FOREIGN KEY `FK_3` (`approveBy`) REFERENCES `Staff` (`staffId`) ON DELETE NO ACTION ON UPDATE CASCADE,
 KEY `FK_4` (`postId`),
 CONSTRAINT `FK_20` FOREIGN KEY `FK_4` (`postId`) REFERENCES `Post` (`postId`) ON DELETE CASCADE ON UPDATE CASCADE,
 KEY `FK_4_1` (`requestBy`),
 CONSTRAINT `FK_20_1` FOREIGN KEY `FK_4_1` (`requestBy`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `RequestDetail`
+CREATE TABLE `RequestCourseDetail`
 (
- `requestDetailId` int NOT NULL ,
- `requestId`       int NOT NULL ,
- `courseId`        int NOT NULL ,
+ `requestCourseDetailId` int NOT NULL ,
+ `requestCourseId`       int NOT NULL ,
+ `courseId`              int NOT NULL ,
 
-PRIMARY KEY (`requestDetailId`),
-KEY `FK_2` (`requestId`),
-CONSTRAINT `FK_17` FOREIGN KEY `FK_2` (`requestId`) REFERENCES `Request` (`requestId`) ON DELETE CASCADE ON UPDATE CASCADE,
+PRIMARY KEY (`requestCourseDetailId`),
+KEY `FK_2` (`requestCourseId`),
+CONSTRAINT `FK_17` FOREIGN KEY `FK_2` (`requestCourseId`) REFERENCES `RequestCourse` (`requestCourseId`) ON DELETE CASCADE ON UPDATE CASCADE,
 KEY `FK_3` (`courseId`),
 CONSTRAINT `FK_18` FOREIGN KEY `FK_3` (`courseId`) REFERENCES `Course` (`courseId`) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -176,7 +198,7 @@ CREATE TABLE `PaymentCheck`
  `approveBy`    int NOT NULL ,
  `joinCourseId` int NOT NULL ,
  `status`       enum('yes','no') NOT NULL ,
- `moneySlip`    varchar(45) NOT NULL ,
+ `imgPath`      varchar(150) NOT NULL ,
 
 PRIMARY KEY (`paymentId`),
 KEY `FK_2` (`userId`),
@@ -187,3 +209,47 @@ KEY `FK_4` (`approveBy`),
 CONSTRAINT `FK_22` FOREIGN KEY `FK_4` (`approveBy`) REFERENCES `Staff` (`staffId`) ON DELETE NO ACTION ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `Report`
+(
+ `reportId`  int NOT NULL ,
+ `requestBy` int NOT NULL ,
+ `courseId`  int NULL ,
+ `postId`    int NULL ,
+ `commentId` int NULL ,
+
+PRIMARY KEY (`reportId`),
+KEY `FK_2` (`requestBy`),
+CONSTRAINT `FK_29` FOREIGN KEY `FK_2` (`requestBy`) REFERENCES `User` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+KEY `FK_3` (`commentId`),
+CONSTRAINT `FK_30` FOREIGN KEY `FK_3` (`commentId`) REFERENCES `Comment` (`commentId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+KEY `FK_4` (`postId`),
+CONSTRAINT `FK_31` FOREIGN KEY `FK_4` (`postId`) REFERENCES `Post` (`postId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+KEY `FK_5` (`courseId`),
+CONSTRAINT `FK_32` FOREIGN KEY `FK_5` (`courseId`) REFERENCES `Course` (`courseId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `RequestVerify`
+(
+ `requestVerifyId` int NOT NULL ,
+ `approveBy`       int NOT NULL ,
+ `status`          enum('waiting', 'approve', 'reject') NOT NULL ,
+ `note`            text NOT NULL ,
+ `imgPath`         varchar(150) NOT NULL ,
+
+PRIMARY KEY (`requestVerifyId`),
+KEY `FK_2` (`approveBy`),
+CONSTRAINT `FK_25_1` FOREIGN KEY `FK_2` (`approveBy`) REFERENCES `Staff` (`staffId`) ON DELETE NO ACTION ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `RequestVerifyDetail`
+(
+ `requestVerifyDetail` int NOT NULL ,
+ `requestVerifyId`     int NOT NULL ,
+ `userId`              int NOT NULL ,
+
+PRIMARY KEY (`requestVerifyDetail`),
+KEY `FK_2` (`requestVerifyId`),
+CONSTRAINT `FK_27` FOREIGN KEY `FK_2` (`requestVerifyId`) REFERENCES `RequestVerify` (`requestVerifyId`) ON DELETE CASCADE ON UPDATE CASCADE,
+KEY `FK_3` (`userId`),
+CONSTRAINT `FK_27_1` FOREIGN KEY `FK_3` (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
