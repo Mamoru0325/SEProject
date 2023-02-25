45ffb51a1c3ae5f46e3f7df0e295e1fefd533cb2
MamoruTFN <78528062+MamoruTFN@users.noreply.github.com>
2023-02-25 22:45:57 +0700

    Update dbdraft1.sql

--- dbdraft1.sql
+++ dbdraft1.sql
@@ -22,7 +22,8 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
+CREATE DATABASE IF NOT EXISTS `se` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `se`;
 --
 -- Table structure for table `account`
 --
