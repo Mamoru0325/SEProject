ab58a89186073c72c86e88ef5777c7580c85a73b
MamoruTFN <78528062+MamoruTFN@users.noreply.github.com>
2023-04-03 21:24:15 +0700

    Update LikePostRepository.java

--- alllearn/src/main/java/eng/cpe/se/project/repository/LikePostRepository.java
+++ alllearn/src/main/java/eng/cpe/se/project/repository/LikePostRepository.java
@@ -14,6 +14,6 @@ public interface LikePostRepository extends CrudRepository<LikePost, Integer> {
 	@Query("select count(lp) from LikePost lp where lp.post = :post")
 	public int countLikePost(@Param("post")Post post);
 	
-	@Query("select lk from LikePost lp where lp.post = :post and lp.user = :user")
+	@Query("select lp from LikePost lp where lp.post = :post and lp.user = :user")
 	public LikePost findByPostAndUser(@Param("post")Post post,@Param("user")User user);
 }
